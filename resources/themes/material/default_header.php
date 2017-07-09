e1a2ad5d9a98b530bcf717c87631ce2ebf4f5888
Jmz <jmztaylor@gmail.com>
2017-07-08 20:36:21 -0500

    DU-ify

---
+++ resources/themes/material/default_header.php
@@ -0,0 +1,15 @@
+<br><br>
+<center>
+<img src="https://i.imgur.com/shV4gfj.png" style='width:90%;' border="0" alt="Null">
+<br><br>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- DU Downloads -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-4828481386057930"
+     data-ad-slot="1478008490"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
